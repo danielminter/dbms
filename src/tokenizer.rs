3483cf60e0cce83de732b022cd1d09b49282c88a
danielminter <danielminter09@gmail.com>
2026-08-02 16:20:42 +1000

    Refactor: Beginnings of a refactor to use a Queue based system

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,187 +1,219 @@
 use std::{
-    fmt::Error,
+    collections::VecDeque,
     io::{self, Error},
+    os::raw::c_int,
 };
 
-fn convert_strings(strings: Vec<&str>) -> Vec<String> {
-    let mut tokens: Vec<String> = Vec::new();
-
-    for mut word in strings {
-        let mut prefix: Vec<Option<char>> = vec![None];
-        let mut suffix: Vec<Option<char>> = vec![None];
-        // Split off the first character if it isn't alphanumeric
-        // Loop until we hit alphanumeric
-        while !word.chars().next().unwrap().is_alphanumeric() {
-            prefix.push(Some(word.chars().next().unwrap()));
-            word = &word[1..];
-        }
-
-        // Split off the last character if it isn't alphabetic
-        // Loop until we hit alphanumeric
-        while !word.chars().last().unwrap().is_alphanumeric() {
-            suffix.insert(0, Some(word.chars().last().unwrap()));
-            word = &word[..(word.len() - 1)];
-        }
-
-        // Push each prefix in order onto the token stack
-        for item in prefix {
-            if let Some(item) = item {
-                tokens.push(String::from(item));
-            }
-        }
-
-        // Push the token onto the token vector
-        tokens.push(String::from(word));
-
-        // Push each suffix in order onto the token stack
-        for item in suffix {
-            if let Some(item) = item {
-                tokens.push(String::from(item));
-            }
-        }
-    }
-
-    tokens
-}
-
-fn convert_to_tokens(strings: Vec<String>) -> Result<Vec<Token>, io::Error> {
-    match strings[0].as_str() {
-        "SELECT" => {
-            return generate_select_command(strings);
-        }
-
-        "INSERT" => {
-            return generate_insert_command(strings);
-        }
-
-        "CREATE" => {
-            return generate_create_command(strings);
-        }
-
-        &_ => todo!(),
-    }
-
-    Ok(vec![])
-}
-
-fn generate_select_command(command: Vec<String>) -> Result<Vec<Token>, io::Error> {
-    // Should never happen but kept as a backstop
-    if command[0] != "SELECT" {
-        return Err(io::Error::new(
-            io::ErrorKind::Other,
-            "Expected SELECT, got something else",
-        ));
-    }
-
-    // Generate an empty select statement
-    let mut statement = SelectStatement::new();
-
-    // Parse the remaining tokens
-    let mut index = 0;
-
-    // Main loop over all of the tokens
-    while index < command.len() {
-        // Consume all tokens up to "FROM"
-        while command[index] != "FROM" {
-            if index >= command.len() {
-                return Err(io::Error::new(
-                    io::ErrorKind::UnexpectedEof,
-                    "No FROM keyword found after SELECT",
-                ));
-            }
-
-            statement.add_column(&command[index]);
-
-            index += 1;
-        }
-
-        // Skip over FROM
-        index += 1;
-
-        // Expect a table name next
-        statement.set_table(&command[index]);
-        index += 1;
-
-        // TODO: Move away from manually incrementing the index.
-        if command[index] == "WHERE" {
-            while command[index] != ";" {
-                let mut clause = Clause::new();
-                index += 1;
-                clause.set_key(&command[index]);
-                index += 1;
-                clause.set_operator(&command[index]);
-                index += 1;
-                clause.set_value(&command[index]);
-                index += 1;
-            }
-        }
-    }
-
-    Ok(vec![])
-}
-
-fn generate_insert_command(command: Vec<String>) -> Result<Vec<Token>, io::Error> {
-    Ok(vec![])
-}
-
-fn generate_create_command(command: Vec<String>) -> Result<Vec<Token>, io::Error> {
-    Ok(vec![])
-}
-
 pub fn tokenize(text: &str) -> Vec<Token> {
-    // Split the text on spaces
-    let split_text = text.split(" ").collect();
-
-    // Seperate symbols from alphanumberic
-    let strings_vector = convert_strings(split_text);
-
-    // Convert the strings to tokens
-    convert_to_tokens(strings_vector)
+    let separated_tokens = seperate_tokens(text).unwrap_or(vec![]);
+    tokenize_strings(separated_tokens)
 }
 
-pub struct Token {
-    token_type: TokenType,
+fn tokenize_strings(strings: Vec<String>) -> Vec<Token> {
+    let mut queue = VecDeque::from(strings);
+    let mut tokens: Vec<Token> = vec![];
+
+    while queue.front() != None {
+        let sequence: Vec<String> = vec![];
+
+        let next = queue.pop_front();
+
+        if next.unwrap().as_str() == "CREATE" {
+            match queue.pop_front().unwrap().as_str() {
+                "TABLE" => {
+                    let token = Token::Keyword(Keyword::new("CREATE TABLE".to_string()));
+                    tokens.push(token);
+                }
+
+                _ => {}
+            }
+        }
+    }
+    vec![]
+}
+
+fn pop_until(mut queue: VecDeque<String>, value: &str) -> (VecDeque<String>, Vec<String>) {
+    let mut slice: VecDeque<String> = VecDeque::new();
+
+    for item in queue {
+        if item != value.to_string() {
+            slice.push_back(item);
+        } else {
+            break;
+        }
+    }
+
+    (VecDeque::new(), vec![])
+}
+
+// TODO: Error Handling
+pub fn seperate_tokens(text: &str) -> Result<Vec<String>, Error> {
+    // convert_strings(split_text)
+    let mut token_strings: Vec<String> = vec![];
+    let characters: Vec<char> = text.chars().collect();
+
+    let mut index: usize = 0;
+
+    // Main parsing loop
+    'parsing: loop {
+        // Break out if we're at the end
+        if index >= characters.len() {
+            break 'parsing;
+        }
+
+        // Initialize a new token
+        let mut current_token: String = String::new();
+
+        // Get the next character in the string
+        let current_char = characters[index];
+
+        // Skip over white space
+        if char::is_whitespace(current_char) {
+            // Increment the index for the next token
+            index += 1;
+            continue 'parsing;
+        }
+
+        if char::is_alphanumeric(current_char) {
+            //Build the current token with all consecutive alphanumeric
+            while index < characters.len() && char::is_alphanumeric(characters[index]) {
+                current_token.push(characters[index]);
+
+                println!("Processing Character: {}", characters[index]);
+
+                // Increment for the next char
+                index += 1;
+            }
+
+            // Store the current token
+            println!("Storing token: {}", &current_token);
+            token_strings.push(current_token.clone());
+
+            // Early exit if we're at the end
+            if index >= characters.len() {
+                break;
+            }
+            continue 'parsing;
+        }
+
+        if !char::is_alphanumeric(current_char) {
+            // Parse symbols
+            // The function handles incrementing the index itself
+            // This is so that it can handle compound symbols like >= and <=
+            current_token = tokenize_non_alphanumeric(&characters, &mut index);
+
+            // Store the token
+            println!("Storing token: {}", &current_token);
+            token_strings.push(current_token.clone());
+        } else {
+            return Err(io::Error::other("Non-parsable character encountered"));
+        }
+    }
+
+    Ok(token_strings)
+}
+
+fn tokenize_non_alphanumeric(characters: &[char], index: &mut usize) -> String {
+    let mut current_token: String;
+    match characters[*index] {
+        '<' => {
+            current_token = String::from("LESSTHAN");
+        }
+        '>' => {
+            current_token = String::from("GREATERTHAN");
+        }
+        '(' => {
+            current_token = String::from("LPAREN");
+        }
+        ')' => {
+            current_token = String::from("RPAREN");
+        }
+        ',' => {
+            current_token = String::from("COMMA");
+        }
+        '*' => {
+            current_token = String::from("STAR");
+        }
+        '=' => {
+            current_token = String::from("EQUALS");
+        }
+        '+' => {
+            current_token = String::from("PLUS");
+        }
+        '-' => {
+            current_token = String::from("MINUS");
+        }
+        ';' => {
+            current_token = String::from("SEMICOLON");
+        }
+        '\'' => {
+            current_token = String::from("SINGLEQUOTE");
+        }
+        '"' => {
+            current_token = String::from("DOUBLEQUOTE");
+        }
+        _ => {
+            current_token = String::from("UNK");
+        }
+    }
+
+    *index += 1;
+
+    // Handle the compound comparisons
+    if index < &mut characters.len() && characters[*index] == '=' {
+        match characters[*index - 1] {
+            '<' | '>' | '+' | '-' => {
+                current_token.push_str("EQUALS");
+                *index += 1;
+            }
+            _ => {}
+        }
+    }
+
+    // Return our constructed token
+    current_token
+}
+
+enum Token {
+    Keyword(Keyword),
+    Identifier(Identifier),
+    StringLit(StringLiteral),
+    IntLit(IntLiteral),
+    Symbol(Symbol),
+}
+
+struct Keyword {
     value: String,
 }
 
-impl Token {
-    pub fn new(token_type: TokenType, value: String) -> Token {
-        Token { token_type, value }
+impl Keyword {
+    fn new(value: String) -> Keyword {
+        Keyword { value }
     }
 }
 
-pub struct CreateStatement {
-    table_name: String,
-    columns: Vec<Column>,
+struct Identifier {
+    value: String,
 }
 
-impl CreateStatement {
-    fn new() -> CreateStatement {
-        CreateStatement {
-            table_name: String::new(),
-            columns: Vec::new(),
-        }
+impl Identifier {
+    fn new(value: String) -> Identifier {
+        Identifier { value }
     }
 }
 
-pub struct InsertStatement {
-    target: String,
-    values: Vec<String>,
+struct StringLiteral {
+    value: String,
 }
 
-impl InsertStatement {
-    fn new() -> InsertStatement {
-        InsertStatement {
-            target: String::new(),
-            values: Vec::new(),
-        }
+impl StringLiteral {
+    fn new(value: String) -> StringLiteral {
+        StringLiteral { value }
     }
 }
 
-pub struct SelectStatement {
-    column_labels: Vec<String>,
-    target: String,
-    clauses: Vec<String>,
+struct IntLiteral {
+    value: c_int,
 }
 
 impl IntLiteral {
